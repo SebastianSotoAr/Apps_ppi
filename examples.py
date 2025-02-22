6e231f978d471a1c7d349560a7feb54471cbf72c
Sebastian Soto Arcila <111089192+SebastianSotoAr@users.noreply.github.com>
2025-02-22 10:51:31 -0500

    Update examples.py

--- examples.py
+++ examples.py
@@ -4,8 +4,17 @@ import streamlit as st
 st.title('Este es el Titulo')
 
 # Info
-st.markdown('Esto es para información importante de la App')
+st.markdown('Esto es información importante de la App')
 
 # Descripción
-st.write('Este escribe texto')
+st.write('Este es texto')
 
+# Opciones de categoría
+categorias = [
+    'Categoría 1',
+    'Categoría 2',
+    'Categoría 3'
+]
+
+# Selección de categoría
+categoria = st.selectbox("Elige una categoría:", categorias)

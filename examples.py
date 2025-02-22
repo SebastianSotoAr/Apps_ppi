87eadb9250b76f799e9d305102543f57a2b38680
Sebastian Soto Arcila <111089192+SebastianSotoAr@users.noreply.github.com>
2025-02-22 10:44:50 -0500

    Create examples.py

---
+++ examples.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Título de la app
+st.title('Este es el Titulo')
+
+# Info
+st.markdown('Esto es para información importante de la App')
+
+# Descripción
+st.write('Escribe texto')
+
